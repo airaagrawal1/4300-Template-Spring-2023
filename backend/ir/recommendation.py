040aee6a60db6a330577859cff91f6ec7f97f2a9
Aira <aa2235@cornell.edu>
2023-04-17 17:17:57 -0400

    add percent confidence to output

--- backend/ir/recommendation.py
+++ backend/ir/recommendation.py
@@ -1,47 +1,59 @@
-import numpy as np 
+import numpy as np
 import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from ir.edit_distance import top_k_edit_distance
 
 # load the data
-with open("./../tea_data.json", "r") as f: 
+with open("./../tea_data.json", "r") as f:
     tea_data = json.load(f)["data"]
 
 # vectorizer
+
+
 def build_tfidf(max_features, stop_words, max_df=0.8, min_df=10, norm="l2"):
-    vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df, max_features=max_features, stop_words=stop_words, norm=norm)
+    vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df,
+                                 max_features=max_features, stop_words=stop_words, norm=norm)
     return vectorizer
-    
+
+
 # data variables
 n_feats = 5000
-tea_to_index = { tea:index for index, tea in enumerate(d["tea_category"] for d in tea_data) }
-index_to_tea = { v:k for k, v in tea_to_index.items() }
+tea_to_index = {tea: index for index, tea in enumerate(
+    d["tea_category"] for d in tea_data)}
+index_to_tea = {v: k for k, v in tea_to_index.items()}
 
 # get the query vector (determine if there is a close match or it is a free-form query)
-def get_query(search_tea, about_vec, review_vec): 
+
+
+def get_query(search_tea, about_vec, review_vec):
     top_k_teas, top_k_dists = top_k_edit_distance(search_tea=search_tea, k=5)
 
     percent_edit = top_k_dists[0] / len(search_tea)
-    threshold = 0.5 # half of the original query had to be edited
-    if percent_edit > threshold: # the query likely does not match any tea 
+    threshold = 0.5  # half of the original query had to be edited
+    if percent_edit > threshold:  # the query likely does not match any tea
         about_query_tfidf = about_vec.transform([search_tea])
         review_query_tfidf = review_vec.transform([search_tea])
-    else: 
+    else:
         tea_index = tea_to_index[top_k_teas[0]]
         search_data = tea_data[tea_index]
         about_query_tfidf = about_vec.transform([search_data["about"]])
-        review_query_tfidf = review_vec.transform([" ".join(search_data["reviews"])])
+        review_query_tfidf = review_vec.transform(
+            [" ".join(search_data["reviews"])])
 
-    return about_query_tfidf, review_query_tfidf, top_k_teas 
+    return about_query_tfidf, review_query_tfidf, top_k_teas
+
 
 def get_recommendations(search_tea, k):
-    about_tfidf_vec = build_tfidf(n_feats, "english") 
-    review_tfidf_vec = build_tfidf(n_feats, "english") 
-    about_tfidf = about_tfidf_vec.fit_transform([d["about"] for d in tea_data]).toarray()
-    review_tfidf = review_tfidf_vec.fit_transform([" ".join(d["reviews"]) for d in tea_data]).toarray()
+    about_tfidf_vec = build_tfidf(n_feats, "english")
+    review_tfidf_vec = build_tfidf(n_feats, "english")
+    about_tfidf = about_tfidf_vec.fit_transform(
+        [d["about"] for d in tea_data]).toarray()
+    review_tfidf = review_tfidf_vec.fit_transform(
+        [" ".join(d["reviews"]) for d in tea_data]).toarray()
 
-    about_query_tfidf, review_query_tfidf, top_k_teas = get_query(search_tea.title(), about_tfidf_vec, review_tfidf_vec)
+    about_query_tfidf, review_query_tfidf, top_k_teas = get_query(
+        search_tea.title(), about_tfidf_vec, review_tfidf_vec)
 
     about_sims = cosine_similarity(about_query_tfidf, about_tfidf).flatten()
     review_sims = cosine_similarity(review_query_tfidf, review_tfidf).flatten()
@@ -52,17 +64,18 @@ def get_recommendations(search_tea, k):
     ranked_ids = (-merged_sims).argsort()
 
     data = []
-    for tea_id in ranked_ids[:k]: 
+    for tea_id in ranked_ids[:k]:
         data.append({
-            "tea_category": tea_data[tea_id]["tea_category"], 
-            "tea_type": tea_data[tea_id]["tea_type"], 
-            "about": tea_data[tea_id]["about"]
+            "tea_category": tea_data[tea_id]["tea_category"],
+            "tea_type": tea_data[tea_id]["tea_type"],
+            "about": tea_data[tea_id]["about"],
+            "brands": tea_data[tea_id]["top_rated_brands"],
+            "score": merged_sims[tea_id]
         })
 
     result = {
-        "data": data, 
+        "data": data,
         "top_k_teas": top_k_teas
     }
 
     return json.dumps(result)
-
