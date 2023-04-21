8d36730d2c32fe4c71119acb917c524362abf37a
Amy Huang <ach243@cornell.edu>
2023-04-21 16:46:14 -0400

    add stuff

--- backend/ir/edit_distance.py
+++ backend/ir/edit_distance.py
@@ -1,9 +1,12 @@
 import json
 import numpy as np
 from nltk.metrics import edit_distance
+import sys
+import os
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # load the data
-with open("../../tea_data.json", "r") as f: 
+with open("./tea_data.json", "r") as f: 
     tea_data = json.load(f)["data"]
 
 # data variables
