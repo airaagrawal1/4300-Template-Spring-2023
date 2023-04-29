2bcda7c2daa24dec97aabe2d35780ee54ab18c81
Aira <aa2235@cornell.edu>
2023-04-29 17:00:35 -0400

    add caffeine content to output

--- backend/ir/recommendation.py
+++ backend/ir/recommendation.py
@@ -10,7 +10,8 @@ with open("./tea_data.json", "r") as f:
 
 # vectorizer
 def build_tfidf(max_features, stop_words, max_df=0.8, min_df=10, norm="l2"):
-    vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df, max_features=max_features, stop_words=stop_words, norm=norm)
+    vectorizer = TfidfVectorizer(min_df=min_df, max_df=max_df,
+                                 max_features=max_features, stop_words=stop_words, norm=norm)
     return vectorizer
 
 # data variables
@@ -20,14 +21,14 @@ tea_to_index = {tea: index for index, tea in enumerate(
 index_to_tea = {v: k for k, v in tea_to_index.items()}
 
 # create a tfidf vector
-def create_vector(data, query): 
+def create_vector(data, query):
     tfidf_vectorizer = build_tfidf(n_feats, "english")
     data_tfidf = tfidf_vectorizer.fit_transform(data).toarray()
     query_tfidf = tfidf_vectorizer.transform(query).toarray()
     return data_tfidf, query_tfidf
 
-# calculate cosine sims 
-def cosine_sims(about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf): 
+# calculate cosine sims
+def cosine_sims(about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf):
     about_sims = cosine_similarity(about_query_tfidf, about_tfidf).flatten()
     review_sims = cosine_similarity(review_query_tfidf, review_tfidf).flatten()
     return about_sims, review_sims
@@ -38,24 +39,31 @@ def get_sim_scores(search_tea, search_description, k):
     review_weight = 0.4
     search_tea_merged_sims = 0
     description_merged_sims = 0
-    if search_tea: 
+    if search_tea:
         top_k_teas, _ = top_k_edit_distance(search_tea=search_tea, k=k)
         search_tea = top_k_teas[0]
         search_data = tea_data[tea_to_index[search_tea]]
-        about_tfidf, about_query_tfidf = create_vector([d["about"] for d in tea_data], [search_data["about"]])
-        review_tfidf, review_query_tfidf = create_vector([" ".join(d["reviews"]) for d in tea_data], [" ".join(search_data["reviews"])])
-        about_sims, review_sims = cosine_sims(about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf)
+        about_tfidf, about_query_tfidf = create_vector(
+            [d["about"] for d in tea_data], [search_data["about"]])
+        review_tfidf, review_query_tfidf = create_vector(
+            [" ".join(d["reviews"]) for d in tea_data], [" ".join(search_data["reviews"])])
+        about_sims, review_sims = cosine_sims(
+            about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf)
         search_tea_merged_sims = about_weight * about_sims + review_weight * review_sims
-        if not search_description: 
+        if not search_description:
             return search_tea_merged_sims, [search_tea]
-    if search_description: 
-        about_tfidf, about_query_tfidf = create_vector([d["about"] for d in tea_data], [search_description])
-        review_tfidf, review_query_tfidf = create_vector([" ".join(d["reviews"]) for d in tea_data], [search_description])
-        about_sims, review_sims = cosine_sims(about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf)
-        description_merged_sims = about_weight * about_sims + review_weight * review_sims
-        if not search_tea: 
+    if search_description:
+        about_tfidf, about_query_tfidf = create_vector(
+            [d["about"] for d in tea_data], [search_description])
+        review_tfidf, review_query_tfidf = create_vector(
+            [" ".join(d["reviews"]) for d in tea_data], [search_description])
+        about_sims, review_sims = cosine_sims(
+            about_tfidf, review_tfidf, about_query_tfidf, review_query_tfidf)
+        description_merged_sims = about_weight * \
+            about_sims + review_weight * review_sims
+        if not search_tea:
             return description_merged_sims, []
-    
+
     search_weight = 0.5
     description_weight = 0.5
 
@@ -63,11 +71,13 @@ def get_sim_scores(search_tea, search_description, k):
 
 # get recommendations
 def get_recommendations(search_tea, search_description, k):
-    sim_scores, search_tea = get_sim_scores(search_tea=search_tea, search_description=search_description, k=k)
+    sim_scores, search_tea = get_sim_scores(
+        search_tea=search_tea, search_description=search_description, k=k)
     ranked_ids = (-sim_scores).argsort()
-    if search_tea != []: 
-        ranked_ids = ranked_ids[ranked_ids != tea_to_index[search_tea[0]]] # remove the current search
-        
+    if search_tea != []:
+        # remove the current search
+        ranked_ids = ranked_ids[ranked_ids != tea_to_index[search_tea[0]]]
+
     data = []
     for tea_id in ranked_ids[:k]:
         data.append({
@@ -75,7 +85,8 @@ def get_recommendations(search_tea, search_description, k):
             "tea_type": tea_data[tea_id]["tea_type"],
             "about": tea_data[tea_id]["about"],
             "brands": tea_data[tea_id]["top_rated_brands"],
-            "score": sim_scores[tea_id] 
+            "caffeine": tea_data[tea_id]["caffeine"],
+            "score": sim_scores[tea_id]
         })
 
     result = {
